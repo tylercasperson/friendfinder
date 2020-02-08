92d6246e5a2a8a34b86d512ffe19706b3ad07b57
Tyler Casperson <tylercasperson@gmail.com>
2020-02-08 00:52:55 -0700

    compares respone with others

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,30 @@ module.exports = function(app){
 
     app.post('/api/friends', function(req, res) {
         var newbie = req.body;
-        friends[Object.keys(friends)[0]].push(newbie);
+        var friendsList = friends[Object.keys(friends)[0]];
+        friendsList.push(newbie);
         res.json(newbie);
+
+        function add(a,b){
+            return a + b;
+        }
+        var newbieResponse = newbie.scores;
+        var compareNewbie = [];
+
+        console.log(friendsList.length);
+        for(var x=0;x<friendsList.length-1;x++){
+            var comparison = [];
+            for(var i = 0; i<newbieResponse.length;i++){
+                if((newbieResponse[i] - friendsList[x].scores[i])<0){
+                    comparison.push((newbieResponse[i] - friendsList[x].scores[i])*-1);
+                } else {
+                    comparison.push((newbieResponse[i] - friendsList[x].scores[i]));
+                }
+            }
+            var totalScore = comparison.reduce(add);
+            compareNewbie.push(totalScore);
+        }
+        console.log(compareNewbie);
+        var bestMatch = Math.min.apply(null, compareNewbie);            
     })
-}
+};
